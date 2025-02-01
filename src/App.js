657fdd5cc1cbf6c8fdb9fcedff9bec11c8f92d19
Alora Riley <alora.riley@gmail.com>
2025-02-01 15:22:24 -0600

    feat: Create AboutMe and Navbar components.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+import Navbar from './components/Navbar';
+import AboutMe from './components/AboutMe';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Navbar/>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>
+          Alora Riley
+        </h1>
       </header>
+      <AboutMe/>
     </div>
   );
 }
---
+++ src/components/AboutMe.js
@@ -0,0 +1,15 @@
+
+const AboutMe = () => {
+
+  return (
+    <section className="aboutMeSection">
+      <h2>
+        About Me Here
+      </h2>
+    </section>
+  )
+
+}
+
+export default AboutMe;
+
