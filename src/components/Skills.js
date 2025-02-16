36cefa10fd90aada99f377c1a077e0cf4a7054f7
Alora Riley <alora.riley@gmail.com>
2025-02-16 17:56:55 -0600

    feat: Add labels for skills icons.

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -7,43 +7,108 @@ const Skills = () => {
   return (
     <section id="skills">
       <h2>
-        Skills Go Here
+        Skills
       </h2>
       <div className="icons">
-        <section className="languages-frameworks">
+        <section className="backend">
           <h3>
-            Languages and Frameworks
+            Backend Languages and Frameworks:
           </h3>
           {<Ruby />}
+          <h4>
+            Ruby
+          </h4>
           {<Rails />} 
-          Active Record
-          SQL
+          <h4>
+            Rails
+          </h4>
+          <h4>
+            Active Record
+          </h4>
+          <h4>
+            SQL
+          </h4>
+        </section>
+        <section className="frontend">
+          <h3>
+            Frontend Languages and Frameworks:
+          </h3>
           {<ReactJS />}
+          <h4>
+            ReactJS
+          </h4>
           {<JavaScript />}
+          <h4>
+            JavaScript
+          </h4>
           {<HTML />}
+          <h4>
+            HTML
+          </h4>
           {<CSS />}
-          TypeScript
+          <h4>
+            CSS
+          </h4>
+          <h4>
+            TypeScript
+          </h4>
         </section>
         <section className="testing-development">
           <h3>
-            Testing and Development
+            Testing and Development:
           </h3>
           {<RSPEC />}
+          <h4>
+            RSpec
+          </h4>
           {<Cypress />}
+          <h4>
+            Cypress
+          </h4>
         </section>
-        <section className="tools-workflow">
+        <section className="workflow">
           <h3>
-            Tools and Workflow:
+            Workflow:
           </h3>
           {<Git />}
+          <h4>
+            Git
+          </h4>
           {<GitHub />}
+          <h4>
+            GitHub
+          </h4>
+          <h4>
+            Jira
+          </h4>
+        </section>
+        <section className="tools">
+          <h3>
+            Tools:
+          </h3>
           {<Postman />}
-          Postico
+          <h4>
+            Postman
+          </h4>
+          <h4>
+            Postico
+          </h4>
           {<Netlify />}
+          <h4>
+            Netlify
+          </h4>
           {<Heroku />}
+          <h4>
+            Heroku
+          </h4>
           {<Miro />}
-          Jira
+          <h4>
+            Miro
+          </h4>
           {<Figma />}
+          <h4>
+            Figma
+          </h4>
         </section>
       </div>
     </section>
