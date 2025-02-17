c5bab68fb78a1e612762ad2317d603f3646d10fc
Alora Riley <alora.riley@gmail.com>
2025-02-16 23:37:35 -0600

    feat: Add first project with styling from skills.

--- src/components/ProjectsContainer.js
+++ src/components/ProjectsContainer.js
@@ -1,10 +1,24 @@
+import JourneyLikeAGenius from '../assets/JourneyLikaAGenius.gif';
+import './ProjectsContainer.css';
+
 const ProjectsContainer = () => {
 
   return (
     <section id="projects">
       <h2>
-        Projects Go Here
+        Projects
       </h2>
+      <section className="project-container">
+        <div classname="single-project">
+          <img src={JourneyLikeAGenius} alt="GIF of Journey Like a Genius Project" />
+          <h3>
+            Journey Like a Genius
+          </h3>
+          <p>
+            This project is about ...
+          </p>
+        </div>
+      </section>
     </section>
   )
 
