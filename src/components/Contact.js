8d955d072c8869b90585c71dd373d246d9b8d9d2
Alora Riley <alora.riley@gmail.com>
2025-02-23 11:45:55 -0600

    feat: Add Contact component and styling.

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,13 +1,33 @@
-const Contact = () => {
+import "./Contact.css";
 
+const Contact = () => {
   return (
     <section id="contact">
-      <h2>
-        Contact Me Here
-      </h2>
+      <div className="contact-content">
+        <div className="contact-photo">
+          <img src="/Portfolio-photo.png" alt="Photo of Alora Riley"/>
+        </div>
+        <div className="contact-form">
+          <h2>Contact Me</h2>
+          <form action="mailto:alora.riley@gmail.com" method="post" enctype="text/plain">
+            <div className="form-group">
+              <label htmlFor="name">Name</label>
+              <input type="text" id="name" name="name" placeholder="Your Name" required />
+            </div>
+            <div className="form-group">
+              <label htmlFor="email">Email</label>
+              <input type="email" id="email" name="email" placeholder="Your Email" required />
+            </div>
+            <div className="form-group">
+              <label htmlFor="message">Message</label>
+              <textarea id="message" name="message" placeholder="Your Message" rows="5" required></textarea>
+            </div>
+            <button type="submit">Send Message</button>
+          </form>
+        </div>
+      </div>
     </section>
-  )
-
-}
+  );
+};
 
 export default Contact;
